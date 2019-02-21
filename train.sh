9161881afa2456a5c5be49157d809f379035326c
Guido Jansen <guido@amidstdata.nl>
2019-02-21 18:01:06 +0100

    Fixed output dir

--- train.sh
+++ train.sh
@@ -3,22 +3,33 @@ train=true
 eval=true
 dotest=false
 dataset="$1"
+pretrained="$2"
 timestamp=$( date +%T )
 
+if [ "$1" -eq "" ]
+  then
+    echo "Please select a dataset (i2b2 or conll)"
+fi
+
+if [ "$2" -eq "" ]
+  then
+    echo "Please select a pretrained model (biobert-pubmed, biobert-pubmed-pmc or bert-base-uncased)"
+fi
+
 export TPU_NAME=biobert-tpu
-export BIOBERT_DIR=gs://biobert-bucket/biobert-pubmed-pmc
+export PRETRAINED_DIR=gs://biobert-bucket/biobert-pubmed-pmc
 export NER_DIR=gs://biobert-bucket/data/$dataset
-export OUTPUT_DIR=gs://biobert-bucket/output/
+export OUTPUT_DIR=gs://biobert-bucket/output/$dataset
 
 python biobert/run_ner.py \
 	   --do_train=$train \
 	   --do_eval=$eval \
 	   --do_test=$dotest \
 	   --use_tpu=true \
-	   --vocab_file=$BIOBERT_DIR/vocab.txt \
-	   --bert_config_file=$BIOBERT_DIR/bert_config.json \
-	   --init_checkpoint=$BIOBERT_DIR/bert_model.ckpt \
+	   --vocab_file=PRETRAINED_DIR/$pretrained/vocab.txt \
+	   --bert_config_file=PRETRAINED_DIR/$pretrained/bert_config.json \
+	   --init_checkpoint=PRETRAINED_DIR/$pretrained/bert_model.ckpt \
 	   --num_train_epochs=3.0 \
 	   --data_dir=$NER_DIR/ \
-	   --output_dir=$OUTPUT_DIR/$dataset_$timestamp \
+	   --output_dir=$OUTPUT_DIR/$timestamp \
 	   --dataset=$dataset
