744b8af1f391cf8e706119fa20d6b73f77498527
Guido Jansen <guido@amidstdata.nl>
2019-03-11 14:49:19 +0100

    Added do_export to python

--- biobert/run_ner.py
+++ biobert/run_ner.py
@@ -73,6 +73,10 @@ flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
 
 flags.DEFINE_bool("do_predict", True,"Whether to run the model in inference mode on the test set.")
 
+flags.DEFINE_bool(
+    "do_export", False,
+    "Whether to export the model.")
+
 flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
 
 flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
